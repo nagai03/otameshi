38aee9fe41fc7af1cc06c595c47a6ddc523c781b
nagai03 <g1344964@cc.kyoto-su.ac.jp>
2015-07-06 01:00:20 +0900

    Revert "Finder.javaの作成"

--- Main.java
+++ Main.java
@@ -4,16 +4,6 @@ public class Main{
     public Main(String[] arguments){
     }
 
-     private Args parseArguments(String[] arguments){
-        Args args = new Args();
-        try {
-            CmdLineParser parser = new CmdLineParser(args);
-            parser.parseArgument(arguments);
-        } catch (CmdLineException e) {
-        }
-        return args;
-    }
-
     public static void main(String[] args){
         new Main(args);
     }
